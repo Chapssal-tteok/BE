665a86d318b8da87e4d5c2756966357a829296e2
jungh150 <jungh150@naver.com>
2025-03-17 19:10:28 +0900

    Initial commit

---
+++ src/test/java/com/chapssal_tteok/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chapssal_tteok.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
