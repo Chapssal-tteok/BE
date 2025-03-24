9d8a946fcfff191b5b79897c31199dae297c089d
jungh150 <jungh150@naver.com>
2025-03-24 21:18:05 +0900

    [#6] feat: user 관련 엔티티 생성

---
+++ src/main/java/com/chapssal_tteok/preview/domain/user/entity/Role.java
@@ -0,0 +1,5 @@
+package com.chapssal_tteok.preview.domain.user.entity;
+
+public enum Role {
+    USER, ADMIN
+}
--- src/main/java/com/chapssal_tteok/preview/domain/user/entity/User.java
+++ src/main/java/com/chapssal_tteok/preview/domain/user/entity/User.java
@@ -2,6 +2,7 @@ package com.chapssal_tteok.preview.domain.user.entity;
 
 import com.chapssal_tteok.preview.global.common.BaseEntity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -17,6 +18,10 @@ public class User extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long id; // 사용자 ID
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role; // 역할 (USER, ADMIN)
 
     @Column(length = 20, nullable = false, unique = true)
     private String username; // 사용자명
@@ -34,4 +39,21 @@ public class User extends BaseEntity {
     private Boolean active = true; // 활성화 여부 (기본값 true)
 
     private LocalDateTime deactivatedAt; // 비활성화 시간
+
+    public User updateRole(Role newRole) {
+        this.role = newRole;
+        return this;
+    }
+
+    public void setName(@Size(min = 2, max = 20, message = "이름은 2~20자 사이여야 합니다.") String name) {
+        this.name = name;
+    }
+
+    public void setUsername(@Size(min = 2, max = 20, message = "사용자명은 2~20자 사이여야 합니다.") String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String encode) {
+        this.password = encode;
+    }
 }
