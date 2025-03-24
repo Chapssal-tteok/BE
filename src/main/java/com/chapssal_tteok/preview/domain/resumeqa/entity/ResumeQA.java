e2767c0d0a60ab5163c95cc92b305f57404ee91d
jungh150 <jungh150@naver.com>
2025-03-24 17:39:54 +0900

    #4 feat: entity 파일 생성

---
+++ src/main/java/com/chapssal_tteok/preview/domain/resumeqa/entity/ResumeQA.java
@@ -0,0 +1,34 @@
+package com.chapssal_tteok.preview.domain.resumeqa.entity;
+
+import com.chapssal_tteok.preview.domain.resume.entity.Resume;
+import com.chapssal_tteok.preview.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ResumeQA extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "resume_qa_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "resume_id")
+    private Resume resume;
+
+    private Long number;
+
+    @Column(columnDefinition = "TEXT")
+    private String question;
+
+    @Column(columnDefinition = "TEXT")
+    private String answer;
+
+    @Column(columnDefinition = "TEXT")
+    private String analysis;
+}
