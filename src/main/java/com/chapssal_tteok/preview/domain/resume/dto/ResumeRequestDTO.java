2079c4770fcc9942926bfcc65ab665253be6ad66
jungh150 <jungh150@naver.com>
2025-03-27 14:56:43 +0900

    [#8] feat: ExistUser 어노테이션 생성

--- src/main/java/com/chapssal_tteok/preview/domain/resume/dto/ResumeRequestDTO.java
+++ src/main/java/com/chapssal_tteok/preview/domain/resume/dto/ResumeRequestDTO.java
@@ -1,5 +1,6 @@
 package com.chapssal_tteok.preview.domain.resume.dto;
 
+import com.chapssal_tteok.preview.global.validation.annotation.ExistUser;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -10,6 +11,7 @@ public class ResumeRequestDTO {
     public static class CreateResumeDTO {
 
         @NotNull(message = "사용자 id는 필수 입력 값입니다.")
+        @ExistUser
         private Long user;
 
         @NotBlank(message = "제목은 필수 입력 값입니다.")
