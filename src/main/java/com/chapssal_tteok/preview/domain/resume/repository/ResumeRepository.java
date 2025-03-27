ebe80029a53504582c77204cb1df34a66dfb3665
jungh150 <jungh150@naver.com>
2025-03-27 15:16:59 +0900

    [#8] feat: resume 관련 레포지토리 생성

---
+++ src/main/java/com/chapssal_tteok/preview/domain/resume/repository/ResumeRepository.java
@@ -0,0 +1,13 @@
+package com.chapssal_tteok.preview.domain.resume.repository;
+
+import com.chapssal_tteok.preview.domain.resume.entity.Resume;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ResumeRepository extends JpaRepository<Resume, Long> {
+
+    Optional<Resume> findById(Long id);
+}
