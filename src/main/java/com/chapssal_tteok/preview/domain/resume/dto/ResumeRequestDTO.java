fcd20f44257e28fd215074cf0c700f6a8b5c381b
jungh150 <jungh150@naver.com>
2025-03-28 16:03:44 +0900

    [#8] fix: resume 생성 api 요청에서 user 삭제

--- src/main/java/com/chapssal_tteok/preview/domain/resume/dto/ResumeRequestDTO.java
+++ src/main/java/com/chapssal_tteok/preview/domain/resume/dto/ResumeRequestDTO.java
@@ -1,18 +1,12 @@
 package com.chapssal_tteok.preview.domain.resume.dto;
 
-import com.chapssal_tteok.preview.global.validation.annotation.ExistUser;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 public class ResumeRequestDTO {
 
     @Getter
     public static class CreateResumeDTO {
-
-        @NotNull(message = "사용자 id는 필수 입력 값입니다.")
-        @ExistUser
-        private Long user;
 
         @NotBlank(message = "제목은 필수 입력 값입니다.")
         private String title;
