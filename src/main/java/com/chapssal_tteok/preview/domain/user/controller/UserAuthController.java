21618f7a0a2f1e0cc02f84171d90591b2ec6551a
Junghwa <jungh150@naver.com>
2025-04-02 02:08:57 +0900

    [#10] minor: user auth api 스웨거 설명 추가

--- src/main/java/com/chapssal_tteok/preview/domain/user/controller/UserAuthController.java
+++ src/main/java/com/chapssal_tteok/preview/domain/user/controller/UserAuthController.java
@@ -5,6 +5,7 @@ import com.chapssal_tteok.preview.domain.user.entity.Role;
 import com.chapssal_tteok.preview.domain.user.service.UserAuthService;
 import com.chapssal_tteok.preview.domain.user.service.UserCommandService;
 import com.chapssal_tteok.preview.global.apiPayload.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,6 +21,7 @@ public class UserAuthController {
     private final UserCommandService userCommandService;
     private final UserAuthService userAuthService;
 
+    @Operation(summary = "회원 가입", description = "새로운 사용자를 등록합니다.")
     @PostMapping("/register")
     public ApiResponse<RegisterResponseDTO> register (@RequestBody @Valid UserRequestDTO.CreateUserDTO userRequestDto){
         RegisterResponseDTO responseDto = userCommandService.registerUser(userRequestDto);
@@ -27,6 +29,7 @@ public class UserAuthController {
         return ApiResponse.onSuccess(responseDto);
     }
 
+    @Operation(summary = "로그인", description = "사용자가 로그인하여 JWT 토큰을 받습니다.")
     @PostMapping("/login")
     public ApiResponse<JwtResponse> login (@RequestBody @Valid LoginRequestDTO loginRequestDto) {
         JwtResponse jwtResponse = userAuthService.login(loginRequestDto);
@@ -34,7 +37,7 @@ public class UserAuthController {
         return ApiResponse.onSuccess(jwtResponse);
     }
 
-
+    @Operation(summary = "리프레시 토큰을 이용한 토큰 갱신", description = "리프레시 토큰을 통해 새로운 엑세스 토큰을 발급받습니다.")
     @PostMapping("/refresh")
     public ApiResponse<JwtResponse> refresh(@RequestBody RefreshRequestDTO refreshRequestDTO) {
         String refreshToken = refreshRequestDTO.getRefreshToken();
@@ -46,6 +49,7 @@ public class UserAuthController {
         return ApiResponse.onSuccess(jwtResponse);
     }
 
+    @Operation(summary = "사용자 권한 변경", description = "관리자만 사용 가능한 API로, 사용자의 권한을 변경합니다.")
     @PatchMapping("/{user_id}/role")
     @PreAuthorize("hasRole('ADMIN')")
     public ApiResponse<UserResponseDTO.UserInfoDTO> changeRole(@PathVariable Long user_id, @RequestParam Role newRole) {
