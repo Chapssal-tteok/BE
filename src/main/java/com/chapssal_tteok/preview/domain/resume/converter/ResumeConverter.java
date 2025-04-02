2a2da165f70b2aa40ddb7d72dae6e8df6aa58496
jungh150 <jungh150@naver.com>
2025-04-02 21:07:40 +0900

    [#10] feat: resume 조회 api 수정

--- src/main/java/com/chapssal_tteok/preview/domain/resume/converter/ResumeConverter.java
+++ src/main/java/com/chapssal_tteok/preview/domain/resume/converter/ResumeConverter.java
@@ -3,11 +3,16 @@ package com.chapssal_tteok.preview.domain.resume.converter;
 import com.chapssal_tteok.preview.domain.resume.dto.ResumeRequestDTO;
 import com.chapssal_tteok.preview.domain.resume.dto.ResumeResponseDTO;
 import com.chapssal_tteok.preview.domain.resume.entity.Resume;
+import com.chapssal_tteok.preview.domain.resumeqa.dto.ResumeQaResponseDTO;
 import com.chapssal_tteok.preview.domain.user.entity.User;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ResumeConverter {
 
     public static ResumeResponseDTO.CreateResumeResultDTO toCreateResumeResultDTO(Resume resume) {
+
         return ResumeResponseDTO.CreateResumeResultDTO.builder()
                 .resumeId(resume.getId())
                 .userId(resume.getUser().getId())
@@ -17,16 +22,32 @@ public class ResumeConverter {
     }
 
     public static ResumeResponseDTO.ResumeDTO toResumeDTO(Resume resume) {
+
+        List<ResumeQaResponseDTO.ResumeQaDTO> resumeQaDTOs = resume.getResumeQas().stream()
+                .map(qa -> ResumeQaResponseDTO.ResumeQaDTO.builder()
+                        .resumeQaId(qa.getId())
+                        .resumeId(qa.getResume().getId())
+                        .number(qa.getNumber())
+                        .question(qa.getQuestion())
+                        .answer(qa.getAnswer())
+                        .analysis(qa.getAnalysis())
+                        .createdAt(qa.getCreatedAt())
+                        .updatedAt(qa.getUpdatedAt())
+                        .build())
+                .collect(Collectors.toList());
+
         return ResumeResponseDTO.ResumeDTO.builder()
                 .resumeId(resume.getId())
                 .userId(resume.getUser().getId())
                 .title(resume.getTitle())
+                .resumeQas(resumeQaDTOs)
                 .createdAt(resume.getCreatedAt())
                 .updatedAt(resume.getUpdatedAt())
                 .build();
     }
 
     public static Resume toResume(ResumeRequestDTO.CreateResumeDTO request, User user) {
+
         return Resume.builder()
                 .user(user)
                 .title(request.getTitle())
--- src/main/java/com/chapssal_tteok/preview/domain/resume/dto/ResumeResponseDTO.java
+++ src/main/java/com/chapssal_tteok/preview/domain/resume/dto/ResumeResponseDTO.java
@@ -1,5 +1,6 @@
 package com.chapssal_tteok.preview.domain.resume.dto;
 
+import com.chapssal_tteok.preview.domain.resumeqa.dto.ResumeQaResponseDTO;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -7,6 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class ResumeResponseDTO {
 
@@ -15,6 +17,7 @@ public class ResumeResponseDTO {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class CreateResumeResultDTO {
+
         private Long resumeId;
         private Long userId;
         private String title;
@@ -27,9 +30,11 @@ public class ResumeResponseDTO {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class ResumeDTO {
+
         private Long resumeId;
         private Long userId;
         private String title;
+        private List<ResumeQaResponseDTO.ResumeQaDTO> resumeQas;
         @JsonFormat(pattern = "yyyy/MM/dd HH:mm:ss")
         private LocalDateTime createdAt;
         @JsonFormat(pattern = "yyyy/MM/dd HH:mm:ss")
