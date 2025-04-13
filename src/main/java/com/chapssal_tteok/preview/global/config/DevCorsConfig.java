aa66b3babd47273277ec3edfea3abaa2e01532b3
jungh150 <jungh150@naver.com>
2025-04-13 15:25:12 +0900

    [#28] fix: WebCongig 파일을 ProdCorsConfig와 DevCorsConfig로 구분

--- src/main/java/com/chapssal_tteok/preview/global/config/DevCorsConfig.java
+++ src/main/java/com/chapssal_tteok/preview/global/config/DevCorsConfig.java
@@ -1,17 +1,19 @@
 package com.chapssal_tteok.preview.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+@Profile("dev") // 개발 환경
+public class DevCorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://172.24.112.1:3000") // 프론트 주소
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOriginPatterns("*") // 개발 시 전체 허용
+                .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
---
+++ src/main/java/com/chapssal_tteok/preview/global/config/ProdCorsConfig.java
@@ -0,0 +1,20 @@
+package com.chapssal_tteok.preview.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Profile("prod") // 운영 환경
+public class ProdCorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://172.24.112.1:3000") // 프론트 주소
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
